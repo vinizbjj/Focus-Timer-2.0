165340d63a9ef2fc2fb93beffe1dac312a0941eb
vinizbjj <viniz.bjj@gmail.com>
2022-09-13 17:25:40 -0300

    iniciando js

---
+++ js/index.js
@@ -0,0 +1,58 @@
+const buttonPlay = document.querySelector('.play')
+const buttonStop = document.querySelector('.stop')
+const buttonVolMore = document.querySelector('.volMore')
+const buttonVolLess = document.querySelector('.volLess')
+
+const buttonSoundForest = document.querySelector('.forestSound')
+const buttonSoundRain = document.querySelector('.rainSound')
+const buttonSoundCoffe = document.querySelector('.coffeSound')
+const buttonSoundFire = document.querySelector('.fireSound')
+
+let minutes
+
+let isFinished = minutes <= 0 && seconds <= 0
+
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+
+
+function countdown() {
+    setTimeout(function () {
+        let seconds = Number(secondsDisplay.textContent)
+        let minutes = Number(minutesDisplay.textContent)
+
+        if (isFinished) {
+            return
+
+        }
+        if (seconds <= 0) {
+            seconds = 4
+
+            minutesDisplay.textContent = String(minutes - 1).padStart(2, '0')
+        }
+
+        secondsDisplay.textContent = String(seconds - 1).padStart(2, '0')
+
+
+
+        countdown()
+
+    }, 1000)
+}
+
+buttonPlay.addEventListener('click', function () {
+    minutes = prompt('Quantos minutos?')
+    minutesDisplay.textContent = String(minutes - 1).padStart(2, '0')
+    countdown()
+
+
+})
+
+buttonStop.addEventListener('click', function () {
+})
+
+buttonVolMore.addEventListener('click', function () {
+})
+
+buttonVolLess.addEventListener('click', function () {
+})
